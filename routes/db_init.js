2c74a172bbe5249f1a2afae9b48f11596d66153d
simon wallin <simon.wallin@eyevinn.se>
2016-12-06 12:29:26 +0100

    fixed init function for db

--- routes/db_init.js
+++ routes/db_init.js
@@ -1,4 +1,4 @@
-console.log("db_init");
+
 
 var mongo = require('mongodb');
 var monk = require('monk');
@@ -13,8 +13,11 @@ var follows = db.collection("follows");
 var seen = db.collection("seen");
 var includes = db.collection("includes");
 
-users.insert({username:"simon wallin", password:sha256("sw0049sw"), email:"simon.wallin@eyevinn.se", admin: true});
-videos.insert({path:"/uploads/test.mp4", length:120, user:"simon wallin", time: now, });
-follows.insert({username: "simon wallin", channel: "news"});
-seen.insert({username:"simon wallin", video: "test"});
-includes.insert({video:"test", channel: "news"});
+
+function init_db(){
+  users.insert({username:"simon wallin", password:sha256("sw0049sw"), email:"simon.wallin@eyevinn.se", admin: true});
+  videos.insert({path:"/uploads/test.mp4", length:120, user:"simon wallin", time: now, });
+  follows.insert({username: "simon wallin", channel: "news"});
+  seen.insert({username:"simon wallin", video: "test"});
+  includes.insert({video:"test", channel: "news"});
+};
