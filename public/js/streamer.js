b0a52fcdbf20f204462372f59ec288f30bb36ddb
simon wallin <simon.wallin@eyevinn.se>
2016-12-14 14:12:39 +0100

    event for fileinput change

--- public/js/streamer.js
+++ public/js/streamer.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
   console.log("streamer script loaded");
+
   $('video').on('ended',function(){
     console.log('Video has ended!');
     console.log($(this).children().first().attr("src"));
     $.post( "/video", { seen: true, video: $(this).children().first().attr("src")} );
   });
 
+  $("input:file").change(function(){
+    console.log("fileinput changed");
+  })
 
 
 
