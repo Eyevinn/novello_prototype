75a28f85ac4a16547a6c3a32b8ea2d82233b3668
simon wallin <simon.wallin@eyevinn.se>
2016-12-15 11:35:49 +0100

    upload now creates 4 different hls streams of uploaded video

--- routes/streamer.js
+++ routes/streamer.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var ffmpeg = require("fluent-ffmpeg");
+var fs = require("fs");
+
+function checkDirectorySync(directory) {
+  try {
+    fs.statSync(directory);
+  } catch(e) {
+    fs.mkdirSync(directory);
+  }
+}
 
 
 
 
 router.get('/', function(req, res, next) {
-  ffmpeg('./video_test/video.mp4')
-    .videoCodec('libx264')
-    .audioCodec('libmp3lame')
-    .size('320x240')
-    .on('error', function(err) {
-      console.log('An error occurred: ' + err.message);
-    })
-    .on('end', function() {
-      console.log('Processing finished !');
-    })
-    .save('./video_test/output.avi');
+  transcode(req.query.input_file, "200");
+  transcode(req.query.input_file, "400");
+  transcode(req.query.input_file, "1000");
+  transcode(req.query.input_file, "1200");
 
 });
 
--- routes/upload.js
+++ routes/upload.js
@@ -5,6 +5,7 @@ var path = require("path");
 var fs = require("fs");
 var monk = require("monk");
 var db = monk('localhost:27017/novello');
+var ffmpeg = require("fluent-ffmpeg");
 
 function checkDirectorySync(directory) {
   try {
@@ -12,6 +13,26 @@ function checkDirectorySync(directory) {
   } catch(e) {
     fs.mkdirSync(directory);
   }
+}
+
+function transcode(file, bitrate){
+  clean_filename = file.split("/").slice(-1);
+  clean_filename = clean_filename[0].split(".").slice(0);
+
+  checkDirectorySync(file.split("/").slice(0,-1).join("/") + "/hls" + bitrate + clean_filename[0]+"/");
+  output_path = file.split("/").slice(0,-1).join("/") + "/hls"+ bitrate + clean_filename[0]+"/" + clean_filename[0] + ".m3u8";
+  ffmpeg(file)
+    .videoCodec('libx264')
+    .audioCodec('libmp3lame')
+    .size('320x240')
+    .videoBitrate(bitrate)
+    .on('error', function(err) {
+      console.log('An error occurred: ' + err.message);
+    })
+    .on('end', function() {
+      console.log('Processing finished !');
+    })
+    .save(output_path);
 }
 
 router.post('/', function(req, res) {
@@ -24,18 +45,17 @@ router.post('/', function(req, res) {
     dir = "./public/uploads/"+req.session.user;
     channel = req.session.channel;
     checkDirectorySync(dir);
-    sampleFile.mv('./public/uploads/' + req.session.user+"/"+ sampleFile.name, function(err) {
+    file = './public/uploads/' + req.session.user+"/"+ sampleFile.name;
+    sampleFile.mv(file, function(err) {
         if (err) {
             res.send("Upload failed" + err);
         }
         else {
             res.send('File uploaded!');
-            cmd.get("pwd", function(data){
-              console.log("path: " + data);
-              command = "ffmpeg -i "+data+"/public/uploads/simon/video.mp4 out.m3u8"
-              cmd.run(command);
-              console.log(command);
-            })
+            transcode(file, "200");
+            transcode(file, "400");
+            transcode(file, "1000");
+            transcode(file, "1200");
         }
     });
     videos.insert({path:'/uploads/' + req.session.user+"/"+ sampleFile.name, length:120, user:req.session.user, time: new Date(), });
