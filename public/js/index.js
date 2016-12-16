8c0efb0e019b1133beb40bbce685d08381d4e2f5
mariliab <marilia.bognandi@gmail.com>
2016-12-16 14:35:29 +0100

    Random color is saved in db when adding new channel, randomcolor is also displayed in view for each channel

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,38 @@
 $(document).ready(function () {
   var activeChannelID;
   var oldChannelID;
-	
+
+	//Popup when there is a video to be uploaded
  $("#fileUpload").on('change', function() {
          console.log('modal ska poppas upp!!!');
          $('#myModal').modal('show');
   });
 
-  $(".channels").click(function() {   	
-  	//channelID = $(this).attr("id");
-  	//console.log(channelID);
+  // Change background color when clicking on a channel
+  $(".channels").click(function() { 
+    console.log($(this).color);  	
   	$(this).css('background-color', 'rgba(0, 0, 0, 0.1)');
 	});
 
+  // Popup - choose which channel to send video to
   $(".channel-list").on('click', function() {
     if (oldChannelID) {
       oldChannelID.removeClass("change-background"); 
     }
+
     var $this = $(this);
     activeChannelID = $(this).attr("id");
 
-
-    $("#change-channelname").val(activeChannelID);
-
-    console.log("You're clicking on: " + activeChannelID); 
     $(this).addClass("change-background");
 
     oldChannelID = $this;
  
     $("#send-to-channel-button").show();
+    $("#change-channelname").val(activeChannelID);
   });
 
   $("#send-to-channel-button").click(function() {  
     channel = activeChannelID;   
-    //$.post( "/upload", { channel: channel } );
     $(".upload-form").submit();
   });
 
